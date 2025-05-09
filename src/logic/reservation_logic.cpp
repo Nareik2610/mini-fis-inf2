bacd6c8e7d85cd7fd3ab46edd4cedea3bd59bfea
Nareik2610 <Nareik.2610@gmx.de>
2025-05-09 14:59:22 +0200

    Update reservation_logic.cpp

--- src/logic/reservation_logic.cpp
+++ src/logic/reservation_logic.cpp
@@ -5,6 +5,12 @@
 void insert_reservation(Reservation const& reservation, std::vector<Reservation>& reservations, std::vector<std::string> const& stops)
 {
     // TODO
+   
+    if (!is_valid_for_route(reservation, stops)) {
+        throw std::invalid_argument("Reservation wurde nicht gefunden.");
+    }
+
+    reservations.push_back(reservation);
 }
 
 Reservation get_next_reservation(std::vector<Reservation> const& reservations, std::string const& next_stop, std::vector<std::string> const& stops)
